1a44624494a50d605da6c6661dd01196a98bcd05
Jimmy DC <jimmydalecleveland@gmail.com>
2021-04-10 17:36:30 -0600

    add thresholds for lh assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -13,9 +13,9 @@ module.exports = {
     assert: {
       assertions: {
         'categories:performance': ['error', { minScore: 0.93 }],
-        // 'categories:accessibility': ['error', { minScore: 1 }],
+        'categories:accessibility': ['error', { minScore: 0.93 }],
         'categories:best-practices': ['error', { minScore: 0.9 }],
-        // 'categories:seo': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
       },
     },
   },
