1f97dbb1f859da61e99a06d5467b1176bd2c90cc
Jimmy DC <jimmydalecleveland@gmail.com>
2021-05-16 15:16:05 -0600

    update lhci to use recommended settings

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,22 +1,23 @@
 module.exports = {
   ci: {
     upload: {
-      target: 'lhci',
-      token: '11378fc3-5660-4681-8ef5-0e8136ff308c',
-      serverBaseUrl: 'https://serene-harbor-65729.herokuapp.com/',
+      target: "lhci",
+      token: "11378fc3-5660-4681-8ef5-0e8136ff308c",
+      serverBaseUrl: "https://serene-harbor-65729.herokuapp.com/",
     },
     collect: {
       numberOfRuns: 3,
-      staticDistDir: './public',
-      url: ['/', '/mock-asynchronous-functions-with-jest/'],
+      staticDistDir: "./public",
+      url: ["/", "/mock-asynchronous-functions-with-jest/"],
     },
     assert: {
+      preset: "lighthouse:recommended",
       assertions: {
-        'categories:performance': ['error', { minScore: 0.93 }],
-        'categories:accessibility': ['error', { minScore: 0.93 }],
-        'categories:best-practices': ['error', { minScore: 0.9 }],
-        'categories:seo': ['error', { minScore: 0.9 }],
+        "categories:performance": ["error", { minScore: 0.93 }],
+        "categories:accessibility": ["error", { minScore: 0.93 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
       },
     },
   },
-}
+};
