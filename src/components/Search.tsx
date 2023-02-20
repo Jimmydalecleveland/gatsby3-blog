76846b4a3937f1ef768d040914ca1f61fa657679
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2023-02-20 15:48:48 -0700

    add intro text and prompts to chat page

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -13,9 +13,7 @@ const Search = ({ posts }: SearchProps) => {
   const [searchResults, setSearchResults] =
     React.useState<SearchResultsProps | null>(null);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(searchValue);
+  const askChat = async (query: string) => {
     setSearchResults(null);
     const result = await fetch(
       "https://flask-production-f494.up.railway.app/chat",
@@ -24,17 +22,16 @@ const Search = ({ posts }: SearchProps) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ query: searchValue }),
+        body: JSON.stringify({ query }),
       }
     ).then((res) => res.json());
-    // const result = await fetch("http://127.0.0.1:8000/chat", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({ query: searchValue }),
-    // }).then((res) => res.json());
+
     setSearchResults(result);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    askChat(searchValue);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -44,15 +41,52 @@ const Search = ({ posts }: SearchProps) => {
   return (
     <div>
       <p>
-        This is an early prototype. I'll be making improvements to it soon but I
-        wanted to get something up live in the meantime for people to try out.
+        Ahoy there, matey! I be the AI chat with the personality of a pirate,
+        ready to help ye find any answer ye seek in this treasure trove of web
+        development knowledge. This digital buccaneer is always at the ready to
+        help ye navigate the rough seas of web development!
       </p>
-      <p>
-        Enter anything into the search box below to ask the AI Chat about my
-        blog.
-      </p>
+      <p>Here be some query suggestions for ye, matey:</p>
+      <ul>
+        <li>
+          <a
+            href="#example1"
+            onClick={() =>
+              askChat("how can I automatically mock an import in jest")
+            }
+          >
+            how can I automatically mock an import in jest
+          </a>
+        </li>
+        <li>
+          <a
+            href="#example2"
+            onClick={() =>
+              askChat(
+                "I'm getting the error: 'NODE_ENV' is not recognized. Pls help."
+              )
+            }
+          >
+            I'm getting the error: 'NODE_ENV' is not recognized. Pls help.
+          </a>
+        </li>
+        <li>
+          <a
+            href="#example3"
+            onClick={() =>
+              askChat("I need help reducing my webpack bundle size")
+            }
+          >
+            I need help reducing my webpack bundle size
+          </a>
+        </li>
+      </ul>
       <form onSubmit={handleSubmit}>
-        <SearchBox value={searchValue} onChange={handleChange} />
+        <SearchBox
+          value={searchValue}
+          onChange={handleChange}
+          placeholder="type your question here..."
+        />
         <button type="submit">Search</button>
       </form>
       {searchResults && (
--- src/components/SearchBox.ts
+++ src/components/SearchBox.ts
@@ -3,7 +3,8 @@ import styled from "@emotion/styled";
 const SearchBox = styled.input`
   width: 100%;
   padding: ${({ theme }) => theme.space.$4};
-  background: ${({ theme }) => theme.colors.$desaturated};
+  background: #203151;
+  color: #dbf2f9;
   font-size: ${({ theme }) => theme.fontSizes.$3};
   border-radius: 5px;
   border: none;
